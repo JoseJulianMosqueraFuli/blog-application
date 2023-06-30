00e73c84b1c162b8ae914e364efbdf0eb411e3af
Jose Mosquera <jojumofu@gmail.com>
2023-06-30 09:17:38 -0500

    fix: modify try exept with shortcut get_object_or_404

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
 
 
 def post_list(request):
-    try:
-        posts = Post.publish.all()
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
-
+    posts = Post.publish.all()
     return render(request, "blog/post/list.xhtml", {"post": posts})
+
+
+def post_detail(request):
+    posts = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, "blog/post/detail.xhtml", {"post": posts})
 
 
 # Create your views here.
