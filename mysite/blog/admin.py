4b39330424236c0323d3e5473033ee0f41c5787e
Jose Mosquera <jojumofu@gmail.com>
2023-06-29 19:43:05 -0500

    feat: customizing post model display in administration view

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    order_by = ["status", "publish"]
+
 
 # Register your models here.
