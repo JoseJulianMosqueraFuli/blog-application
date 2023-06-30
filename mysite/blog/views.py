c8414adea3bbffa15d67408c23bca2e1f3e50a4f
Jose Mosquera <jojumofu@gmail.com>
2023-06-30 09:07:09 -0500

    feat: add list and detail first view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.publish.all()
+    return render(request, "blog/post/list.xhtml", {"post": posts})
+
 
 # Create your views here.
