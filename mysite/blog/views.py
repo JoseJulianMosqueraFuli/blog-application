9ef409bf4ae1ba9132fc0f3d106d96cb21d3da07
Jose Mosquera <jojumofu@gmail.com>
2023-06-30 09:09:19 -0500

    feat: add try exception to firts view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import Http404
 
 
 def post_list(request):
-    posts = Post.publish.all()
+    try:
+        posts = Post.publish.all()
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+
     return render(request, "blog/post/list.xhtml", {"post": posts})
 
 
