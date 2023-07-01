e41b563a58fe33f5bfaa7e00d75b54d59a68b9b5
Jose Mosquera <jojumofu@gmail.com>
2023-07-01 18:52:53 -0500

    feat: handling pagination errors

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)  # Show 3 posts per page
     page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If the page is out of range (e.g. 9999),
+        posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.xhtml", {"posts": posts})
 
 
@@ -21,6 +27,3 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
     return render(request, "blog/post/detail.xhtml", {"post": post})
-
-
-# Create your views here.
