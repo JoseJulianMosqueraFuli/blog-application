a01de5100b3eebd70c2bc507216cc589697ab14c
Jose Mosquera <jojumofu@gmail.com>
2023-07-07 09:37:55 -0500

    feat: create a template tag that returns a query set with most commented post

--- mysite/blog/templatetags/blogs_tags.py
+++ mysite/blog/templatetags/blogs_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
 
 register = template.Library()
 
@@ -7,3 +8,10 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count("comments")).order_by(
+        "-total_comments"
+    )[:count]
