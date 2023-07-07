fb7351822240577b3594a5ba72ee4d8e22934e21
Jose Mosquera <jojumofu@gmail.com>
2023-07-07 09:06:27 -0500

    feat: retrieve posts by similatiry

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,6 +8,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 # from dotenv import load_dotenv
 
@@ -58,10 +59,20 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm
+    posts_tags_id = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=posts_tags_id).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
     return render(
         request,
         "blog/post/detail.xhtml",
-        {"post": post, "comments": comments, "form": form},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
