dc61f00b88584cde6481b9944eb73f5be80204e1
Jose Mosquera <jojumofu@gmail.com>
2023-07-13 15:34:03 -0500

    feat: Create Search query simple view, form and template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,15 @@
 import os
 
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .models import Post
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 # from dotenv import load_dotenv
 
@@ -110,4 +111,24 @@ def post_comment(request, post_id):
         request,
         "blog/post/comment.xhtml",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body")
+            ).filter(search=query)
+
+    return render(
+        request,
+        "blog/post/search.xhtml",
+        {"form": form, "query": query, "results": results},
     )
